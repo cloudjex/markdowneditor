6b65ea5c4d612e5d05cf5717fb4f22d9f9a41a60
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-20 13:03:30 +0900

    dev

---
+++ .github/fastapi_terraform/apigateway.tf
@@ -0,0 +1,54 @@
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name = "cloudjex-apigateway"
+  endpoint_configuration {
+    types = ["EDGE"]
+  }
+  tags = {
+    Name : "cloudjex"
+  }
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.fastapi_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda]
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = "api"
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fastapi_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  value = "https://${aws_api_gateway_rest_api.rest_api.id}.execute-api.us-east-1.amazonaws.com/api"
+}
---
+++ .github/fastapi_terraform/lambda.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "fastapi_lambda" {
+  function_name    = "cloudjex-lambda"
+  role             = "arn:aws:iam::736798815711:role/cloudjex-lambda"
+  runtime          = "python3.13"
+  handler          = "app.handler"
+  filename         = "${path.module}/../build/deploy.zip"
+  source_code_hash = filebase64sha256("${path.module}/../build/deploy.zip")
+  timeout          = 30
+  tags = {
+    Name : "cork-up"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.fastapi_lambda.function_name}"
+  retention_in_days = 30
+  tags = {
+    Name : "cloudjex"
+  }
+}
