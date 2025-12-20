1248b5b46df97f2d8c5cf83a513996e9bfdcbf06
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-20 14:16:16 +0900

    dev

--- .github/fastapi_terraform/apigateway.tf
+++ .github/fastapi_terraform/apigateway.tf
@@ -69,9 +69,9 @@ resource "aws_api_gateway_base_path_mapping" "custom_mapping" {
 }
 
 output "api_endpoint" {
-  value = "https://${aws_api_gateway_rest_api.rest_api.id}.execute-api.us-east-1.amazonaws.com/api"
+  value = "https://${aws_api_gateway_rest_api.rest_api.id}.execute-api.us-east-1.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}"
 }
 
 output "custom_api_url" {
-  value = "https://${aws_api_gateway_domain_name.custom_domain.domain_name}"
+  value = "https://${aws_api_gateway_domain_name.custom_domain.domain_name}/${aws_api_gateway_stage.stage.stage_name}"
 }
