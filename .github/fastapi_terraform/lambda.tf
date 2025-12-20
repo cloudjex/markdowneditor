6045d81608cf5b69e0e6a4a6bcdba4079dce8c27
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-20 15:30:00 +0900

    dev

--- .github/fastapi_terraform/lambda.tf
+++ .github/fastapi_terraform/lambda.tf
@@ -1,6 +1,37 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.project_name}-lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "logs_full_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
 resource "aws_lambda_function" "fastapi_lambda" {
   function_name    = "${var.project_name}-lambda"
-  role             = "arn:aws:iam::736798815711:role/${var.project_name}-lambda"
+  role             = aws_iam_role.lambda_exec_role.arn
   runtime          = "python3.13"
   handler          = "app.handler"
   filename         = "${path.module}/../build/deploy.zip"
