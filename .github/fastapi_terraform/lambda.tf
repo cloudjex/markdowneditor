c2305a38f710045face5a596787dd36671fd6cb3
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-29 20:07:24 +0900

    done #30

--- .github/fastapi_terraform/lambda.tf
+++ .github/fastapi_terraform/lambda.tf
@@ -19,14 +19,53 @@ resource "aws_iam_role" "lambda_exec_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "logs_full_access" {
+resource "aws_iam_role_policy_attachment" "logs_basic_access" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+resource "aws_iam_role_policy" "dynamodb_rw" {
+  name = "${var.project_name}-dynamodb-rw"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ],
+        Resource = [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ses_basic_email" {
+  name = "${var.project_name}-ses-send-email"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_lambda_function" "fastapi_lambda" {
