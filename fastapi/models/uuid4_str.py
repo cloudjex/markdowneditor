7e07552ba6debf5f8df7e5d2360a78ddb0f6aafa
yuutakatagiri <yuutakatagiri9@gmail.com>
2026-02-15 14:43:08 +0900

    update open api

--- fastapi/models/uuid4_str.py
+++ fastapi/models/uuid4_str.py
@@ -1,6 +1,4 @@
 pattern = {
-    "title": "UUID",
     "description": "uuid v4",
     "examples": ["00000000-0000-0000-0000-000000000000"],
-    "pattern": r"^[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}$",
 }
