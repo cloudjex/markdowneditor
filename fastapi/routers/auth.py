3184320013faf054acfd17ca42c362fb98c8130c
yuutakatagiri <yuutakatagiri9@gmail.com>
2026-02-22 14:24:31 +0900

    tmp

--- fastapi/routers/auth.py
+++ fastapi/routers/auth.py
@@ -1,9 +1,11 @@
+import secrets
 import config
 from db.dynamodb import DynamoDBClient
 from fastapi import APIRouter, Depends
 from models import req
 from models.jwt import IdToken, JwtClaim
 from models.result import Result
+from models.user import User
 from utilities import errors
 from utilities.bcrypt_hash import Bcrypt
 from utilities.jwt_client import JwtClient
@@ -58,6 +60,35 @@ async def func(
 
     id_token = JwtClient().encode(jwt.email, req.group_id)
     return {"id_token": id_token}
+
+
+@router.post(
+    path="/signup",
+    summary="Sign up",
+    response_model=Result,
+    responses={
+        409: config.RES_409,
+        422: config.RES_422,
+    },
+)
+async def func(
+    req: req.SignUp,
+):
+    if db_client.get_user(req.email) is not None:
+        raise errors.ConflictError
+
+    user = User(
+        email=req.email,
+        password=Bcrypt().hash(req.password),
+        groups=[],
+        options={
+            "enabled": False,
+            "otp": f"{secrets.randbelow(1000000):06d}",
+        },
+    )
+
+
+    return {"result": "success"}
 
 
 @router.post(
